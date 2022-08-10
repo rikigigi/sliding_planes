19bd2874ed9842bbf8359937ce8a9435020994ac
Riccardo Bertossa <rbertoss@sissa.it>
2022-08-10 15:27:30 +0200

    added average calculation, inserted all convenient moves to explore all the phase space

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,8 @@ fn read_number<T : std::str::FromStr >() -> T {
 
 enum McMove {
     SingleShift{dx: f64, idx : usize},
-    PairShift{dx: f64, idx : usize}
+    PairShift{dx: f64, idx : usize},
+    TripletShift{dx : f64, idx : usize},
 }
 
 struct PlaneSystem {
@@ -47,6 +48,9 @@ impl PlaneSystem {
        }
    }
 
+   fn abc(&self) -> [f64;5] {
+      self.x.map(|a| a-((a+0.5)/3.0).floor()*3.0)
+   }
 
    fn v(&self) -> (f64,f64,f64) {
        let (mut v0,mut v1,mut v2) = (0.0,0.0,0.0);
@@ -65,6 +69,10 @@ impl PlaneSystem {
        v1+=vs.1;
        v2+=vs.2;  
        (v0,v1,v2)
+   }
+   
+   fn dv_dt(&self) -> f64 {
+      self.p.calc(vp(self.t-self.x[4])).1
    }
 
    fn move_dv(&self, m : &Option<McMove>) -> Option<f64> {
@@ -98,6 +106,21 @@ impl PlaneSystem {
                      ),
                  _ => None
               }
+          },
+          Some(McMove::TripletShift{dx,idx}) => {
+              let idx = *idx;
+              match idx {
+                  0 => Some ( //moving planes x_1 and x_2 and x_3.
+                        self.p.calc(vp(self.x[idx]+dx)).0-self.p.calc(vp(self.x[idx])).0 + self.p.calc(vp(self.x[idx+3]-(self.x[idx+2]+dx) )).0 - self.p.calc(vp(self.x[idx+3]-(self.x[idx+2]) )).0
+                       ),
+                 1 => Some ( // x_2, x_3 and x_4
+                       self.p.calc(vp(self.x[idx]+dx-(self.x[idx-1]) )).0 - self.p.calc(vp(self.x[idx]-(self.x[idx-1]) )).0 + self.p.calc(vp(self.x[idx+2]-(self.x[idx+1]+dx) )).0 - self.p.calc(vp(self.x[idx+2]-(self.x[idx+1]) )).0 
+                       ),
+                 2 => Some ( // x_3, x_4, x_5, but the last one interact with the first fixed one due to pbc
+                       self.p.calc(vp(self.x[idx]+dx-(self.x[idx-1]) )).0 - self.p.calc(vp(self.x[idx]-(self.x[idx-1]) )).0 +  self.p.calc(vp(self.t-(self.x[idx+2]+dx))).0 - self.p.calc(vp(self.t-self.x[idx+2])).0
+                     ),
+                 _ => None
+              }
           }
           None => None
        }
@@ -121,30 +144,72 @@ impl PlaneSystem {
                         },
                     _ => None
                },
+           Some(McMove::TripletShift{dx,idx}) =>
+                match idx {
+                    0..=2 =>  { 
+                           self.x[idx]+=dx; self.x[idx+1]+=dx; self.x[idx+2]+=dx;
+                           m
+                        },
+                    _ => None
+               },
            None => None
       }
    }
 }
 
 fn generate_random_move(rng : &mut rand::rngs::ThreadRng, dxmax : f64) -> Option<McMove> {
-    let moveChoice = Uniform::from(0..2);
+    let moveChoice = Uniform::from(0..3);
     let planeChoice = Uniform::from(0..5);
     let dxChoice = Uniform::from(-dxmax..dxmax);
     let idx = moveChoice.sample(rng);
+    let add = (rng.gen_range(0..=2) as f64)*2.0;
     match idx {
         0 => Some(McMove::SingleShift{
-                dx : dxChoice.sample(rng),
+                dx : dxChoice.sample(rng)+add,
                 idx : planeChoice.sample(rng)
              }),
         1 => Some(McMove::PairShift{
-                dx : dxChoice.sample(rng),
-                idx : planeChoice.sample(rng)
+                dx : dxChoice.sample(rng)+add,
+                idx : planeChoice.sample(rng)%4
+             }),
+        2 => Some(McMove::PairShift{
+                dx : dxChoice.sample(rng)+add,
+                idx : planeChoice.sample(rng)%3
              }),
         _ => None
     }
 }
 
+struct OnlineAverage {
+    mean : f64,
+    M2 : f64,
+    count : u64
+}
 
+impl OnlineAverage {
+   fn new() -> OnlineAverage {
+     OnlineAverage{
+       mean: 0.0,
+       M2: 0.0,
+       count: 0
+     }
+   }
+   fn update(&mut self,newValue : f64) {
+      self.count+=1;
+      let delta = newValue - self.mean;
+      self.mean += delta/self.count as f64;
+      let delta2 = newValue - self.mean;
+      self.M2 += delta*delta2
+   }
+   fn get_mean_variance_count(&self) -> (f64,f64,u64) {
+      if self.count >= 2 {
+         (self.mean,self.M2/self.count as f64, self.count)
+      } else {
+         (self.mean,self.M2,self.count)
+      }
+   }
+
+}
 
 fn main() {
 
@@ -161,6 +226,10 @@ fn main() {
     let mut system = PlaneSystem::new(p,t, beta);
     let mut rng = rand::thread_rng();
     let rnd_01 = Uniform::from(0.0..1.0);
+
+    let mut v_mean = OnlineAverage::new();
+    let mut de_mean = OnlineAverage::new();
+    let mut acc_mean = OnlineAverage::new();
 
     for jstep in 0..nsteps_out { 
        let mut accepted = 0;
@@ -186,10 +255,20 @@ fn main() {
           }
        }
        let v = system.v();
-       println!("{} {} {} {} {} {} {} {} {}",
-            accepted as f64 / (accepted + rejected) as f64,
-            v.0, v.1, v.2,
-            system.x[0],system.x[1],system.x[2],system.x[3], system.x[4] )
+       let fupper = system.dv_dt();
+       let abc = system.abc();
+       let accr=accepted as f64 / (accepted + rejected) as f64;
+       v_mean.update(v.0);
+       de_mean.update(fupper);
+       acc_mean.update(accr);
+       println!("{} {} {} {} {} {} {} {} {} {} {} {} {} {} {}",
+            accr,
+            fupper, v.0, v.1, v.2,
+            system.x[0],system.x[1],system.x[2],system.x[3], system.x[4],
+            abc[0],abc[1],abc[2],abc[3],abc[4] );
     }
+    println!("#!v {} {} {}",v_mean.get_mean_variance_count().0, v_mean.get_mean_variance_count().1, v_mean.get_mean_variance_count().2);
+    println!("#!dv_dt {} {} {}",de_mean.get_mean_variance_count().0, de_mean.get_mean_variance_count().1, de_mean.get_mean_variance_count().2);
+    println!("#!accr {} {} {}",acc_mean.get_mean_variance_count().0, acc_mean.get_mean_variance_count().1, acc_mean.get_mean_variance_count().2);
 
 }
